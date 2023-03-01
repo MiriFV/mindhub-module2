48c5e3bc14c98f2c2479f236eb68eef4f773f282
Miranda Frias <mirifv.96@gmail.com>
2023-03-01 12:42:55 -0300

    solucionado :100:

--- assets/scripts/upcoming.js
+++ assets/scripts/upcoming.js
@@ -1,29 +1,34 @@
-const contenedor = document.getElementById('contenedorUp')
+
+const contenedorUp = document.querySelector('#contenedorUp')
+
 
 const currentDate = data.currentDate;
 const events = data.events;
 
-let Tarjetas = crearCardUp(events,currentDate);
-function crearCardUp(events, currentDate);
+let Tarjetas = ''
+
+function crearCard(eventos,currentDate)
   {   let coleccionEventos=''
-      for(const evento of events){
-        if(evento.date > currentDate)
-      coleccionEventos += `<div class="col ">
-                      <div class="card h-100 " style="height:35rem,weight: 20rem">
+      for(const evento of eventos){
+        if(evento.date>currentDate){
+        coleccionEventos += `<div class="col ">
+                      <div class="card h-100 " style="height:30rem">
                           <img src="${evento.image}" class="card-img-top" alt="...">
                       <div class="card-body">
                       <h5 class="card-title">${evento.name}</h5>
                       <p class="card-subtitle">${evento.date}</p>
                       <p class="card-text">${evento.description}</p>
-                      <p class="card-text">Capacidad: ${evento.capacity}</p>
                       </div>
-                      <div class="card-footer row row-cols-2">
-                     <div class="col"><small class="text-muted">Asistencia: ${evento.price}</small></div>
+                      <div class="card-footer row row-cols-2"><div class="col"><small class="text-muted">Price: $${evento.price}</small></div>
                       <div class="col"><a href="./details.html"><button class="btn btn-outline-light btn-dark" type="submit">Ver mas</button></a></div>
                       </div>
-    </div>
-  </div>`
+                      </div>
+                      </div>`
+      }
+      
    }
     return coleccionEventos;
 }
+
+Tarjetas = crearCard(events,currentDate);
 contenedorUp.innerHTML=Tarjetas;
